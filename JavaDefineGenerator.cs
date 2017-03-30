bd86023ddf80993fe96c33b1b422a3daa5b7ac67
Benjamin <Benjamin.Zhou@morningstar.com>
2017-03-30 17:45:33 +0800

    fix some bugs

--- JavaDefineGenerator.cs
+++ JavaDefineGenerator.cs
@@ -41,7 +41,7 @@ namespace excel2json
             {
                 FieldDef field;
 
-                var fieldName = column.ToString();
+                var fieldName = column.ToString().Trim();
                 if (fieldName.StartsWith("R_"))
                 {
                     fieldName = fieldName.Replace("R_", "");
@@ -64,8 +64,8 @@ namespace excel2json
                 }
 
                 field.name = fieldName;
-                field.type = typeRow[column].ToString();
-                field.comment = commentRow[column].ToString();
+                field.type = typeRow[column].ToString().Trim();
+                field.comment = commentRow[column].ToString().Trim();
 
                 m_fieldList.Add(field);
             }
@@ -98,6 +98,43 @@ namespace excel2json
             string defName = Path.GetFileNameWithoutExtension(filePath);
 
             //-- 创建代码字符串
+            StringBuilder sbForEntity = new StringBuilder();
+            StringBuilder sbForEntityToString = new StringBuilder();
+            sbForEntity.AppendLine("package Models;");
+            sbForEntity.AppendLine();
+            sbForEntity.AppendLine("import java.util.*;");
+            sbForEntity.AppendLine();
+            sbForEntity.AppendFormat("public class {0}\r\n{{", defName + "Entity");
+            sbForEntity.AppendLine();
+            foreach (FieldDef field in m_fieldList)
+            {
+                sbForEntity.AppendFormat("\tpublic {0} {1}; // {2}", ConvertJavaDataType(field.type), field.name, field.comment);
+
+                sbForEntityToString.AppendLine("\t\tif(" + field.name + " == null){sb.append(\"" + field.name + "=null<br>\");} else {sb.append(\"" + field.name + " = \" + " + field.name + ".toString() + \"<br>\");}");
+
+                //sbForEntityToString.AppendLine("\t\tsb.append(\"" + field.name + "=\" + " + field.name + " == null ? \"null\" : "+ field.name + ".toString() + \"\\n\");");
+                sbForEntity.AppendLine();
+            }
+            sbForEntity.AppendLine();
+            sbForEntity.AppendLine();
+            sbForEntity.AppendLine("\t@Override");
+            sbForEntity.AppendLine("\tpublic String toString() {");
+            sbForEntity.AppendLine("\t\tStringBuilder sb=new StringBuilder();");
+            sbForEntity.Append(sbForEntityToString.ToString());
+            sbForEntity.AppendLine("\t\treturn sb.toString();");
+            sbForEntity.AppendLine("\t}");
+            sbForEntity.AppendLine("}");
+            sbForEntity.AppendLine();
+            //-- 保存文件
+            String filePath1 = filePath.Replace(defName, defName + "Entity");
+            using (FileStream file = new FileStream(filePath1, FileMode.Create, FileAccess.Write))
+            {
+                using (TextWriter writer = new StreamWriter(file, encoding))
+                    writer.Write(sbForEntity.ToString());
+            }
+
+
+
             StringBuilder sb = new StringBuilder();
             sb.AppendLine("package Models;");
             sb.AppendLine();
@@ -108,22 +145,11 @@ namespace excel2json
             sb.AppendLine();
             if (this.ClassComment != null)
                 sb.AppendLine(this.ClassComment);
-            sb.AppendFormat("class {0}\r\n{{", defName + "Entity");
-            sb.AppendLine();
-
-            foreach (FieldDef field in m_fieldList)
-            {
-                sb.AppendFormat("\tpublic {0} {1}; // {2}", ConvertJavaDataType(field.type), field.name, field.comment);
-                sb.AppendLine();
-            }
-
-            sb.AppendLine("}");
-            sb.AppendLine();
 
             sb.AppendLine("public class " + defName + " implements DataPoint {");
             sb.AppendLine();
             sb.AppendLine("\tprivate String TestCasePath;");
-            sb.AppendLine("\tprivate List<" + defName + "Entity" + "> TestCaseEntities;");
+            sb.AppendLine("\tpublic List<" + defName + "Entity" + "> TestCaseEntities;");
             sb.AppendLine("\tprivate HashMap map;");
             sb.AppendLine();
             sb.AppendLine("\tpublic " + defName + "(String testcasepath) {");
@@ -138,7 +164,7 @@ namespace excel2json
             sb.AppendLine("\t\tif (size > 0) {");
             sb.AppendLine("\t\t\tfor (int i = 0; i < size; i++) {");
             sb.AppendLine("\t\t\t\t" + defName + "Entity entity = TestCaseEntities.get(i);");
-            sb.AppendLine("\t\t\t\t//TODO: Write Code at here.");
+            sb.AppendLine("\t\t\t\t//TODO: Write Code at here.Save data into DynamoDB or Kinesis");
             sb.AppendLine();
             sb.AppendLine();
             sb.AppendLine("\t\t\t}");
@@ -160,6 +186,39 @@ namespace excel2json
             sb.AppendLine("\t\t}");
             sb.AppendLine("\t\treturn map;");
             sb.AppendLine("\t}");
+
+
+            sb.AppendLine("\t@Override");
+            sb.AppendLine("\tpublic String OutputTestResult(HashMap<Integer, Boolean> result)");
+            sb.AppendLine("\t{");
+            sb.AppendLine("\t\tStringBuilder sb = new StringBuilder();");
+            sb.AppendLine("\t\tsb.append(\" <div><table border='1' cellspacing='0' bordercolor='#000000' width = '100%' style='border-collapse:collapse;'><tr><td colspan='3'>\" + this.getClass().getSimpleName() + \"</td></tr>\"); ");
+            sb.AppendLine("\t\tString Format = \" <tr><td>%s</td><td>%s</td><td>%s</td></tr>\";");
+
+            sb.AppendLine("\t\tList<Integer> list = new ArrayList<>(result.keySet());");
+            sb.AppendLine("\t\tfor (int i = 0; i < list.size(); i++)");
+            sb.AppendLine("\t\t{");
+            sb.AppendLine("\t\t\tint No = list.get(i);");
+            sb.AppendLine("\t\t\tfor (int j = 0; j < result.size(); j++)");
+            sb.AppendLine("\t\t\t{");
+            sb.AppendLine("\t\t\t\t"+ defName + "Entity entity = TestCaseEntities.get(j);");
+            sb.AppendLine("\t\t\t\tif (No == entity.No)");
+            sb.AppendLine("\t\t\t\t{");
+            sb.AppendLine("\t\t\t\t\tsb.append(String.format(Format, entity.No, result.get(entity.No), entity.toString()));");
+            sb.AppendLine("\t\t\t\t}");
+            sb.AppendLine("\t\t\t}");
+            sb.AppendLine("\t\t}");
+            sb.AppendLine("\t\tsb.append(\"</table></div> \");");
+            sb.AppendLine("\t\treturn sb.toString();");
+            sb.AppendLine("\t}");
+
+
+
+
+
+
+
+
             sb.AppendLine();
             sb.AppendLine();
             sb.AppendLine("}");
